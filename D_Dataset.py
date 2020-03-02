e644e5b20ebe0935d9dde311cc0cd0e8221efc0d
zee-ashraff <61715010+zee-ashraff@users.noreply.github.com>
2020-03-02 18:03:20 -0500

    Update D_Dataset.py

--- D_Dataset.py
+++ D_Dataset.py
@@ -4,22 +4,19 @@ import glob
 from sklearn.utils import shuffle
 import numpy as np
 
-#function that takes in train_path, image_size and classes, and returns images, labels and cls
 def load_train(train_path, image_size, classes):
     images = []
-    image_names = []
-    cls = []
     labels = []
+    img_names = []
+    cls = []
 
-    print('Reading training images')
-    #go through the list
+    print('Going to read training images')
     for fields in classes:
         index = classes.index(fields)
-        print('Reading {} files (Index: {})'.format(fields, index))
+        print('Now going to read {} files (Index: {})'.format(fields, index))
         path = os.path.join(train_path, fields, '*g')
         files = glob.glob(path)
         for fl in files:
-            #read and process the image
             image = cv2.imread(fl)
             image = cv2.resize(image, (image_size, image_size),0,0, cv2.INTER_LINEAR)
             image = image.astype(np.float32)
@@ -29,24 +26,24 @@ def load_train(train_path, image_size, classes):
             label[index] = 1.0
             labels.append(label)
             flbase = os.path.basename(fl)
-            image_names.append(flbase)
+            img_names.append(flbase)
             cls.append(fields)
     images = np.array(images)
     labels = np.array(labels)
-    image_names = np.array(image_names)
+    img_names = np.array(img_names)
     cls = np.array(cls)
 
-    return images, labels, image_names, cls
+    return images, labels, img_names, cls
 
-#instantiates a dataset objects with all required parameters
+
 class DataSet(object):
 
-  def __init__(self, images, labels, image_names, cls):
+  def __init__(self, images, labels, img_names, cls):
     self._num_examples = images.shape[0]
 
     self._images = images
     self._labels = labels
-    self._image_names = image_names
+    self._img_names = img_names
     self._cls = cls
     self._epochs_done = 0
     self._index_in_epoch = 0
@@ -60,8 +57,8 @@ class DataSet(object):
     return self._labels
 
   @property
-  def image_names(self):
-    return self._image_names
+  def img_names(self):
+    return self._img_names
 
   @property
   def cls(self):
@@ -76,44 +73,43 @@ class DataSet(object):
     return self._epochs_done
 
   def next_batch(self, batch_size):
-    #Returns next batch_size examples from data set
+    """Return the next `batch_size` examples from this data set."""
     start = self._index_in_epoch
     self._index_in_epoch += batch_size
 
     if self._index_in_epoch > self._num_examples:
-      # updated after each epoch
+      # After each epoch we update this
       self._epochs_done += 1
       start = 0
       self._index_in_epoch = batch_size
       assert batch_size <= self._num_examples
     end = self._index_in_epoch
 
-    return self._images[start:end], self._labels[start:end], self._image_names[start:end], self._cls[start:end]
+    return self._images[start:end], self._labels[start:end], self._img_names[start:end], self._cls[start:end]
 
-#function to read train dataset
+
 def read_train_sets(train_path, image_size, classes, validation_size):
   class DataSets(object):
     pass
   data_sets = DataSets()
 
-  images, labels, image_names, cls = load_train(train_path, image_size, classes)
-  images, labels, image_names, cls = shuffle(images, labels, image_names, cls)
+  images, labels, img_names, cls = load_train(train_path, image_size, classes)
+  images, labels, img_names, cls = shuffle(images, labels, img_names, cls)
 
   if isinstance(validation_size, float):
     validation_size = int(validation_size * images.shape[0])
 
-  training_images = images[validation_size:]
-  training_labels = labels[validation_size:]
-  train_image_names = image_names[validation_size:]
-  train_cls = cls[validation_size:]
-
   validation_images = images[:validation_size]
   validation_labels = labels[:validation_size]
-  validation_image_names = image_names[:validation_size]
+  validation_img_names = img_names[:validation_size]
   validation_cls = cls[:validation_size]
 
-  data_sets.train = DataSet(training_images, training_labels, train_image_names, train_cls)
-  data_sets.valid = DataSet(validation_images, validation_labels, validation_image_names, validation_cls)
+  train_images = images[validation_size:]
+  train_labels = labels[validation_size:]
+  train_img_names = img_names[validation_size:]
+  train_cls = cls[validation_size:]
 
-#return the train and validation sets
+  data_sets.train = DataSet(train_images, train_labels, train_img_names, train_cls)
+  data_sets.valid = DataSet(validation_images, validation_labels, validation_img_names, validation_cls)
+
   return data_sets
